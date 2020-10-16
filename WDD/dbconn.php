00bff124a4d2f305dbadf5b2b12ee146393f41f1
emoie-lk <72979524+emoie-lk@users.noreply.github.com>
2020-10-16 20:41:41 +0530

    Update dbconn.php

--- WDD/dbconn.php
+++ WDD/dbconn.php
@@ -1,4 +1,4 @@
 <?php
-     $conn = mysqli_connect("localhost", "root", "", "wiliam") 
+     $conn = mysqli_connect("us-cdbr-east-02.cleardb.com", "b1421f9040c5ca", "c6a54f55", "heroku_ee69a9c396c762c") 
      or die("Could not connect to the database");
 ?>
